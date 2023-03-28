2fb3ef971fdc9f11582438d8cddb2596a08abb3a
hmimeee <hmimeee@gmail.com>
2023-03-28 20:17:40 +0600

    Added exception and correspondant messages

--- src/Easysendsms.php
+++ src/Easysendsms.php
@@ -5,6 +5,7 @@ namespace Hmimeee\Easysendsms;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
 use Hmimeee\Easysendsms\Messages\EasysendsmsMessage;
+use Hmimeee\Easysendsms\Exceptions\EasysendsmsException;
 
 class Easysendsms
 {
@@ -36,19 +37,44 @@ class Easysendsms
     /**
      * Send the message
      * 
-     * @return true
+     * @return void
      */
     public function send()
     {
-        Http::baseUrl($this->baseUrl)->timeout(6)->post('bulksms', [
+        $response = Http::baseUrl($this->baseUrl)->post('bulksms', [
             'username' => $this->username,
             'password' => $this->password,
             'from' => $this->from,
             'to' => $this->to,
             'text' => $this->message->content,
             'type' => $this->message->type,
-        ]);
+        ])->json();
 
-        return true;
+        if (is_integer($response)) {
+            throw new EasysendsmsException($this->getErrorMessage($response));
+        }
+    }
+
+    /**
+     * Get the exception message return
+     * 
+     * @param mixed $code
+     * @return string
+     */
+    public function getErrorMessage($code)
+    {
+        $errors = [
+            "1001" => "Invalid URL. This means that one of the parameters was not provided or left blank.",
+            "1002" => "Invalid username or password parameter.",
+            "1003" => "Invalid type parameter.",
+            "1004" => "Invalid message.",
+            "1005" => "Invalid mobile number.",
+            "1006" => "Invalid sender name.",
+            "1007" => "Insufficient credit.",
+            "1008" => "Internal error (do NOT re-submit the same message again).",
+            "1009" => "Service not available (do NOT re-submit the same message again)."
+        ];
+
+        return $errors[$code] ?? 'Unknow error occured';
     }
 }
